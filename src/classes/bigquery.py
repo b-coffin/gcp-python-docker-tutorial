faec086e85e8b2688b4f6f03eed9e9b92582b8d4
Kyoya Takei <kyoya-takei@dac.co.jp>
2024-03-16 15:16:26 +0900

    feat: jsonl取得関数を REPEATED mode に対応させる

--- src/classes/bigquery.py
+++ src/classes/bigquery.py
@@ -25,23 +25,25 @@ class Bigquery:
     def get_columnsjsonl(self, full_tableid):
         columnsjsonl = {}
         for schemafield in self.get_schemafields(full_tableid):
-            if schemafield.field_type == "RECORD":
-                child_jsonl = {}
-                for f in schemafield.fields:
-                    child_jsonl[f.name] = self.get_defaultvalue(schemafield.field_type)
-                columnsjsonl[schemafield.name] = [child_jsonl]
+            if schemafield.mode == "REPEATED":
+                columnsjsonl[schemafield.name] = [self.get_defaultvalue(schemafield)]
             else:
-                columnsjsonl[schemafield.name] = self.get_defaultvalue(schemafield.field_type)
+                columnsjsonl[schemafield.name] = self.get_defaultvalue(schemafield)
 
         return columnsjsonl
 
 
-    def get_defaultvalue(self, fieldtype):
-        if fieldtype in ["NUMERIC", "INTEGER"]:
+    def get_defaultvalue(self, schemafield):
+        if schemafield.field_type in ["NUMERIC", "INTEGER"]:
             return 0
-        elif fieldtype in ["DATE"]:
+        elif schemafield.field_type in ["DATE"]:
             return "9999-12-31"
-        elif fieldtype in ["BOOLEAN"]:
+        elif schemafield.field_type in ["BOOLEAN"]:
             return "TRUE"
+        elif schemafield.field_type in ["RECORD"]:
+            temp_json = {}
+            for f in schemafield.fields:
+                temp_json[f.name] = self.get_defaultvalue(f)
+            return temp_json
         else:
             return ""
